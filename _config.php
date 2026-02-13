0010ccdfcce01da84e67b3ad028aaac2873ca420
れ <maziwarota0123@gmail.com>
2026-02-14 01:56:02 +0900

    _config.php を更新

--- _config.php
+++ _config.php
@@ -1,20 +1,26 @@
-<?php 
-$conn = mysqli_connect("localhost", 'root' , '', "anime") or die("Connection fail");
+<?php
+$db_host = getenv('DB_HOST') ?: "localhost";
+$db_user = getenv('DB_USER') ?: "root";
+$db_pass = getenv('DB_PASS') ?: "";
+$db_name = getenv('DB_NAME') ?: "anime";
+$db_port = getenv('DB_PORT') ?: "3306";
 
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, (int)$db_port)
+  or die("Connection fail");
 
-$websiteTitle = "Zoro";
+$websiteTitle = getenv('WEBSITE_TITLE') ?: "Zoro";
+$contactEmail = getenv('CONTACT_EMAIL') ?: "@gmail.com";
+$version = getenv('APP_VERSION') ?: "0.2";
+
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";
 $websiteLogo = $websiteUrl . "/files/images/logo_zoro.png";
-$contactEmail = "@gmail.com";
-
-$version = "0.2";
 
 $discord = "https://dsc.gg/kirixen";
 $github = "https://github.com/kirixen";
 $twitter = "https://x.com/KiriX3n";
- 
+
 $disqus = "https://.disqus.com/embed.js";
-$api = ""; 
+$api = getenv('API_ENDPOINT') ?: "";
 
 $banner = $websiteUrl . "/files/images/banner.png";
 ?>
