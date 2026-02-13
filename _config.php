d2c176eb714a2e38013e8e7609a5d1513ce98656
れ <maziwarota0123@gmail.com>
2026-02-14 02:00:08 +0900

    _config.php を更新

--- _config.php
+++ _config.php
@@ -1,19 +1,19 @@
 <?php
-$db_host = getenv('DB_HOST') ?: "localhost";
-$db_user = getenv('DB_USER') ?: "root";
-$db_pass = getenv('DB_PASS') ?: "";
-$db_name = getenv('DB_NAME') ?: "anime";
-$db_port = getenv('DB_PORT') ?: "3306";
+$db_host = getenv('DB_HOST') ?: '127.0.0.1';
+$db_port = (int)(getenv('DB_PORT') ?: 3306);
+$db_user = getenv('DB_USER') ?: 'root';
+$db_pass = getenv('DB_PASS') ?: '';
+$db_name = getenv('DB_NAME') ?: 'anime';
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, (int)$db_port)
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port)
   or die("Connection fail");
 
+// 以降は既存設定のままでOK
 $websiteTitle = getenv('WEBSITE_TITLE') ?: "Zoro";
-$contactEmail = getenv('CONTACT_EMAIL') ?: "@gmail.com";
-$version = getenv('APP_VERSION') ?: "0.2";
-
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";
 $websiteLogo = $websiteUrl . "/files/images/logo_zoro.png";
+$contactEmail = getenv('CONTACT_EMAIL') ?: "@gmail.com";
+$version = getenv('APP_VERSION') ?: "0.2";
 
 $discord = "https://dsc.gg/kirixen";
 $github = "https://github.com/kirixen";
